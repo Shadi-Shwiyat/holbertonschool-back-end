23ebbc13ec14337f49500214dd8070aa64d3c304
Shadi-Shwiyat <raz611103@gmail.com>
2023-07-21 16:13:55 -0700

    Function exports all user todo data to a json file

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
 """ This script uses REST API to retrieve the task completed
-    by a given employee ID, then exports it to a csv file """
+    by all employees, then exports it to a json file """
 
 import json
 import requests
-from sys import argv
-
 
 def toDo():
-    """ Function exports todo list to a csv file """
-    emp_id = int(argv[1])
-    todo_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
-    id_response = requests.get(todo_url)
+    """ Function exports all employees to a json file """
+    todo_list = []
+    todo_dict = {}
 
-    if id_response.status_code == 200:
-        todo_data = id_response.json()
-        done_tasks = sum(1 for item in todo_data if item["completed"])
-        total_tasks = len(todo_data)
+    emp_url = "https://jsonplaceholder.typicode.com/users"
+    emp_response = requests.get(emp_url)
 
-        emp_name_url = "https://jsonplaceholder.typicode.com/users"
-        name_response = requests.get(emp_name_url)
+    if emp_response.status_code == 200:
+        emp_data = emp_response.json()
 
-        if name_response.status_code == 200:
-            name_data = name_response.json()
-            for user in name_data:
-                if user["id"] == emp_id:
-                    usr_name = user["username"]
+        for item in emp_data:
+            emp_id = item['id']
+            usr_name = item['username']
+
+            todo_url = f"https://jsonplaceholder.typicode.com" \
+                       f"/users/{emp_id}/todos"
+            todo_response = requests.get(todo_url)
+
+            if todo_response.status_code == 200:
+                todo_data = todo_response.json()
+
+                for item in todo_data:
+                    todo_list.append({
+                        "username": usr_name,
+                        "task": item['title'],
+                        "completed": item['completed']
+                    })
+                todo_dict[emp_id] = todo_list
+
+            else:
+                print("Error: Unable to fetch todo data.")
+
         else:
-            print("Error: Unable to fetch data.")
+            print("Error: Unable to fetch user data.")
 
-        todo_list = []
-        for item in todo_data:
-            todo_list.append({
-                "task": item['title'],
-                "completed": item['completed'],
-                "username": usr_name
-            })
-
-        todo_dict = {emp_id: todo_list}
-
-        with open(f'{emp_id}.json', mode='w', newline='') as file:
-            json.dump(todo_dict, file)
-
-    else:
-        print("Error: Unable to fetch data.")
+    with open(f'todo_all_employees.json', mode='w', newline='') as file:
+        json.dump(todo_dict, file)
 
 
 if __name__ == "__main__":
